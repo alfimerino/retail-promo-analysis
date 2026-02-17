04b02335bd664b5b60da7b9fddde7fee6fd29338
Alfredo Merino <alfimerino@gmail.com>
2026-02-16 23:41:38 -0500

    MJ-107: Develop a single page website to view project report findings (#14)
    * MJ-106: Initialized React and Vite
    
    * MJ-107: Added PDF content to iFrame
    
    * MJ-107: Fixed paths for ReadMe Images

---
+++ web/Majin_2025_Spring_Retrospective/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <iframe
+      src="/2025_Spring_Retrospective_Report.pdf"
+      style={{ width: '100vw', height: '100vh', border: 'none' }}
+      title="Spring 2025 Retrospective Report"
+    />
+  )
+}
+
+export default App
